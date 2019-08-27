ecea96da401e2191be27b3328262a5abd2f9492c
DrewEhlers11 <drewehlers11@gmail.com>
2019-08-26 18:47:10 -0700

    FizzBuzz
    Fizz Buzz challenge on HackerRank, could not make a shorter solution then the one on there.

---
+++ FizzBuzz/FizzBuzz.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0)
+			cout << "Fizz\n";
+
+		else if (i % 5 == 0)
+			cout << "Buzz\n";
+
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			cout << "FizzBuzz\n";
+
+		else
+			cout << i << "\n";
+	}
+	return 0;
+}
